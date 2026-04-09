3ba75cb9e3c06d33a3041f0d13c466251771eff0
kittyboy06 <afsalahmed2006@gmail.com>
2026-04-09 09:23:24 +0530

    T31

--- src/T31_In1.java
+++ src/T31_In1.java
@@ -1,31 +1,41 @@
 import static java.lang.System.out;
 import java.util.Scanner;
 
-public class T31_In1 {
-    public static void main(String[] args) {
+public class T31_In1
+{
+    public static void main(String[] args)
+    {
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int s = 1; s <= n - i; s++) {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int s = 1; s <= n - i; s++)
+            {
                 out.print("  ");
             }
 
-            for (int j = 1; j <= 2 * i - 1; j++) {
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
                 out.print("* ");
             }
 
             out.println();
         }
 
-        for (int i = n - 1; i >= 1; i--) {
-            for (int s = 1; s <= n - i; s++) {
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int s = 1; s <= n - i; s++)
+            {
                 out.print("  ");
             }
 
-            for (int j = 1; j <= 2 * i - 1; j++) {
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
                 out.print("* ");
             }
+
+            out.println();
         }
     }
 }
